16a6afa4602c12f5333611e76b837eefccda92ad
Vladimir Pozdnyakov <pozdnakovvladimir088@gmail.com>
2024-05-25 23:23:30 +0300

    done

---
+++ run_shadow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -B
+
+from time import time
+from common.tk_drawer import TkDrawer
+from shadow.polyedr import Polyedr
+
+
+tk = TkDrawer()
+try:
+    for name in ["test1", "test2", "test3", "test4", "box"]:
+        print("=============================================================")
+        print(f"Начало работы с полиэдром '{name}'")
+        start_time = time()
+        Polyedr(f"data/{name}.geom").draw(tk)
+        delta_time = time() - start_time
+        s = Polyedr(f"data/{name}.geom").sum_spec()
+        print(f"Сумма проекций = {round(s, 10)}")
+        print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        input("Hit 'Return' to continue -> ")
+except (EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
